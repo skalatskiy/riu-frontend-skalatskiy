43e0aafeb709e05474e879dac2f7cfc6c22486de
Alexander Skalatskiy <alexander.skalatskiy@riskiq.net>
2025-05-28 16:53:14 +0200

    feature - hero page - Routed and adapted the new hero page to the rest of the site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroPageComponent } from './heroes/hero-page/hero-page.component';
 
 export const routes: Routes = [
-    { path: 'heroes', component: HeroesComponent },
     { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: "heroes/:heroId", component: HeroPageComponent },
 ];
