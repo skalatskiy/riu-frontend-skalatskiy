dbe9590da64d5028e9a8228d79e1c6c09654c9dc
Alexander Skalatskiy <alexander.skalatskiy@riskiq.net>
2025-05-28 13:18:13 +0200

    feature - Heroes list - Assure the new heroes page is the main one

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
@@ -18,12 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('RIU-Frontend-Skalatskiy');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, RIU-Frontend-Skalatskiy');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'heroes', component: HeroesComponent },
+    { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+];
